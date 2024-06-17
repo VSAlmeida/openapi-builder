0aed29d28682d880e34c0e403d42873e7815d8bc
VSAlmeida <vsalmeida@ibm.com>
2024-06-17 08:58:37 -0300

    feat: add home logo, description and start button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import { ThemeContext } from '@context/ThemeContext';
+
+import LogoVerticalDark from '@assets/logoVerticalDark.svg?react';
+import LogoVerticalLight from '@assets/logoVerticalLight.svg?react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { theme } = useContext(ThemeContext);
+
+  const { t } = useTranslation();
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="home">
+      {theme === 'dark' && <LogoVerticalDark className="home--logo" />}
+      {theme === 'light' && <LogoVerticalLight className="home--logo" />}
+      <div>
+        <b className="home--title">OpenAPI Builder</b> {t('home.itsFree')}{' '}
+        <b>{t('home.noAccount')}</b>
+      </div>
+      <div>{t('home.description')}</div>
+      <button
+        type="button"
+        className="home--button"
+        onClick={() => {
+          navigate('/builder');
+        }}
+      >
+        {t('home.button')}
+      </button>
+    </div>
+  );
 };
 
 export default Home;
